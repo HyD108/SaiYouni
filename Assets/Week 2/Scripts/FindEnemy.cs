f1a30a527507edbe64551166dca78339d34e1f14
HyD108 <163080196+HyD108@users.noreply.github.com>
2024-10-07 15:10:38 +0900

    week 2

--- Assets/Week 2/Scripts/FindEnemy.cs
+++ Assets/Week 2/Scripts/FindEnemy.cs
@@ -1,19 +1,36 @@
-//using System.Collections;
-//using System.Collections.Generic;
-//using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-//public class FindEnemy : MonoBehaviour
-//{
-//    //----------------------Edit below here --------------------
-//    public Enemy FindEnemyWithSmallestHealth()
-//    {
-//        return null;
-//    }
+public class FindEnemy : MonoBehaviour
+{
+    public List<Enemy> enemies;
+    //----------------------Edit below here --------------------
+    public Enemy FindEnemyWithSmallestHealth()
+    {
+        Enemy minHealthEnemy = enemies[0];
+        foreach (Enemy enemy in enemies)
+        {
+            if (enemy.health < minHealthEnemy.health)
+            {
+                minHealthEnemy = enemy;
+            }
+        }
+        return minHealthEnemy;
+    }
 
-//    public Enemy FindEnemyWithLargestHealth()
-//    {
-//        return null;
-//    }
+    public Enemy FindEnemyWithLargestHealth()
+    {
+        Enemy maxHealthEnemy = enemies[0];
+        foreach (Enemy enemy in enemies)
+        {
+            if (enemy.health > maxHealthEnemy.health)
+            {
+                maxHealthEnemy = enemy;
+            }
+        }
+        return maxHealthEnemy;
+    }
 
-//    //----------------------Edit above here --------------------
-//}
+    //----------------------Edit above here --------------------
+}
