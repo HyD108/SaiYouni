c804824eb7e47a9b646a1667d436802d0bf91f45
HyD108 <163080196+HyD108@users.noreply.github.com>
2024-10-07 15:19:16 +0900

    week 2

--- Assets/Week 2/Scripts/HealthRandom.cs
+++ Assets/Week 2/Scripts/HealthRandom.cs
@@ -7,24 +7,28 @@ public class HealthRandom : MonoBehaviour
 {
     //----------------------Edit below here --------------------
     public Slider slider;
-    public int maxHealth = 0;
-    public int minHealth = 100;
+    public int maxHealth = 100;
+    public int minHealth = 0;   
     //----------------------Edit above here --------------------
+
     public void Start()
     {
         slider.minValue = minHealth;
         slider.maxValue = maxHealth;
+        RandomHealth();
     }
 
     // On Click function
     public void OnClick()
     {
-        RandomHealth();
+        RandomHealth(); 
     }
 
     //----------------------Edit below here --------------------
     public void RandomHealth()
     {
-
+        int randomHealth = Random.Range(minHealth, maxHealth + 1);
+        slider.value = randomHealth;
     }
+    //----------------------Edit above here --------------------
 }
